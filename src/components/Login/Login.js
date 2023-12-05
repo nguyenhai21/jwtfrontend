f1115cd9223deee976838408b6d1244fe7b5b2cf
hai <tienhai2182001@gmail.com>
2023-12-05 14:09:16 +0700

    init

---
+++ src/components/Login/Login.js
@@ -0,0 +1,10 @@
+const Login = (props) => {
+    return (
+        <div className="login-contanier">
+            login
+            <button>Test bootstrap</button>
+        </div>
+    )
+}
+
+export default Login;
