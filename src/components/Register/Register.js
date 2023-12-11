1596b44a6b2d61f1c9a0b15c24759f535230cb02
hai <tienhai2182001@gmail.com>
2023-12-11 14:48:52 +0700

    init 1

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,10 +1,18 @@
 import './Register.scss';
 import { useHistory } from "react-router-dom";
+import axios from "axios";
+import { useEffect } from 'react';
+
 const Register = (props) => {
     let history = useHistory();
     const handleLoginAccount = () => {
         history.push("/login");
     }
+    useEffect(() => {
+        axios.get("http://localhost:8080/api/test-api").then(data => {
+            console.log('check data axios: ', data)
+        })
+    }, []);
 
     return (
         <div className="register-container ">
